db6c177adb08df99449982e9226fd6d2f6a2d841
Andrew Dobson <tandrewlopez@gmail.com>
2022-12-07 11:31:48 -0500

    added cors to project to resolve clientside fetching issue

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const path = require("path");
 const morgan = require("morgan");
+const cors = require("cors");
 
 const app = express();
 
 app.use(morgan("dev"));
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
