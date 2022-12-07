75df4ae41089e6dc47c2be9eb22ed5ef30fb6f0d
Andrew Dobson <tandrewlopez@gmail.com>
2022-12-06 23:17:18 -0500

    Created dummy data to seed db with 100 users.

--- db/seed.js
+++ db/seed.js
@@ -1,23 +1,13 @@
 const { conn, User } = require("./");
-
-const USERS = [
-  {
-    username: "administrator",
-    password: "adminPassword",
-    email: "admin@genRecords.com",
-    firstName: "Andrew",
-    lastName: "Dobson",
-    isAdmin: true,
-  },
-];
+const { randomUsers, specificUsers } = require("./dummyData.json");
 
 const seed = async () => {
   try {
     console.log("Seeding started.");
     await conn.sync({ force: true });
-
     //DO THE THINGS
-    const users = await Promise.all(USERS.map((user) => User.create(user)));
+    await Promise.all(specificUsers.map((user) => User.create(user)));
+    await Promise.all(randomUsers.map((user) => User.create(user)));
     console.log("Seeding successful.");
   } catch (error) {}
 };
