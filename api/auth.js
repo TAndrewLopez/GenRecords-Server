6687d496d454e2f1eba7d26cb6838843c08f6259
Andrew Dobson <tandrewlopez@gmail.com>
2022-12-17 12:20:31 -0500

    update user api handle unentered inputs,

--- api/auth.js
+++ api/auth.js
@@ -26,7 +26,14 @@ router.put("/:userId", requireToken, async (req, res, next) => {
   try {
     const user = await User.findByPk(req.user.id);
     const { firstName, lastName, username, email, password, img } = req.body;
-    await user.update({ firstName, lastName, username, email, password, img });
+    await user.update({
+      firstName: firstName || req.user.firstName,
+      lastName: lastName || req.user.lastName,
+      username: username || req.user.username,
+      email: email || req.user.email,
+      password: password || req.user.password,
+      img: img || req.user.img,
+    });
     res.json({ user });
   } catch (error) {
     next(error);
