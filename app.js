1dd4073facf952a636b52b10ce45179399d69d39
Andrew Dobson <tandrewlopez@gmail.com>
2022-12-21 23:06:45 -0500

    stripe integrated with payment route

--- app.js
+++ app.js
@@ -5,10 +5,40 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(morgan("dev"));
+const stripe = require("stripe")(process.env.STRIPE_SECRET_TEST);
+
 app.use(cors());
+app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.post("/payment", async (req, res) => {
+  const { amount, id } = req.body;
+  try {
+    const payment = await stripe.paymentIntents.create({
+      amount,
+      currency: "USD",
+      description: "Generational Records",
+      payment_method: id,
+      confirm: true,
+    });
+
+    console.log("payment", payment);
+    res.json({
+      message: "Payment successful",
+      success: true,
+    });
+  } catch (error) {
+    console.log("error", error);
+
+    res.status(error.statusCode).json({
+      message: error.message,
+      success: false,
+      code: error.code,
+      decline_code: error.decline_code,
+    });
+  }
+});
 
 //PROJECT ROUTES
 app.use("/api", require("./api"));
