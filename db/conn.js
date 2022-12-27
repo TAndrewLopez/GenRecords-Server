01dfdf38513fbceffbd0e669009a7cd28992e4e2
Andrew Dobson <tandrewlopez@gmail.com>
2022-12-27 12:14:53 -0500

    test 4

--- db/conn.js
+++ db/conn.js
@@ -1,23 +1,52 @@
 const Sequelize = require("sequelize");
 
-const config = {
+// const config = {
+//   dialect: "postgres",
+//   dialectOptions: {
+//     ssl: process.env.DATABASE_URL
+//       ? { require: true, rejectUnauthorized: false }
+//       : false,
+//   },
+//   logging: false,
+// };
+
+// const conn = new Sequelize(
+//   process.env.DATABASE_URL || "postgres://localhost:5432/recordShop",
+//   null,
+//   null,
+//   config
+// );
+
+const conn = new Sequelize({
+  database: process.env.DATABASE,
+  username: process.env.DBUSERNAME,
+  password: process.env.DBPASSWORD,
+  host: process.env.DBHOST,
+  port: 5432,
   dialect: "postgres",
-  logging: false,
-};
-
-if (process.env.DATABASE_URL) {
-  config.dialectOptions = {
-    ssl: process.env.DATABASE_URL
-      ? { require: true, rejectUnauthorized: false }
-      : false,
-  };
-}
-
-const conn = new Sequelize(
-  process.env.DATABASE_URL || "postgres://localhost:5432/recordShop",
-  null,
-  null,
-  config
-);
+  dialectModule: pg,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
 
 module.exports = conn;
+
+// const conn = new Sequelize({
+//   database: "genRecords_server",
+//   username: "tandrewlopez",
+//   password: "FREE123",
+//   host: "mydatabase-instance.cyrkvzihboei.us-east-1.rds.amazonaws.com",
+//   port: 5432,
+//   dialect: "postgres",
+//   dialectModule: pg,
+//   dialectOptions: {
+//     ssl: {
+//       require: true,
+//       rejectUnauthorized: false,
+//     },
+//   },
+// });
