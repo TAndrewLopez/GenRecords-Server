5b7ed63ba047cfca627d4a7d6717400cc5e55e73
Andrew Dobson <tandrewlopez@gmail.com>
2022-12-27 15:50:52 -0500

    revert some changes

--- db/conn.js
+++ db/conn.js
@@ -1,37 +1,37 @@
 const Sequelize = require("sequelize");
-const pg = require("pg");
+// const pg = require("pg");
 
-// const config = {
-//   dialect: "postgres",
-//   dialectOptions: {
-//     ssl: process.env.DATABASE_URL
-//       ? { require: true, rejectUnauthorized: false }
-//       : false,
-//   },
-//   logging: false,
-// };
-
-// const conn = new Sequelize(
-//   process.env.DATABASE_URL || "postgres://localhost:5432/recordShop",
-//   null,
-//   null,
-//   config
-// );
-
-const conn = new Sequelize({
-  database: process.env.DATABASE,
-  username: process.env.DBUSERNAME,
-  password: process.env.DBPASSWORD,
-  host: process.env.DBHOST,
-  port: 5432,
+const config = {
   dialect: "postgres",
-  dialectModule: pg,
   dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    },
+    ssl: process.env.DATABASE_URL
+      ? { require: true, rejectUnauthorized: false }
+      : false,
   },
-});
+  logging: false,
+};
+
+const conn = new Sequelize(
+  process.env.DATABASE_URL || "postgres://localhost:5432/recordShop",
+  null,
+  null,
+  config
+);
+
+// const conn = new Sequelize({
+//   database: process.env.DATABASE,
+//   username: process.env.DBUSERNAME,
+//   password: process.env.DBPASSWORD,
+//   host: process.env.DBHOST,
+//   port: 5432,
+//   dialect: "postgres",
+//   dialectModule: pg,
+//   dialectOptions: {
+//     ssl: {
+//       require: true,
+//       rejectUnauthorized: false,
+//     },
+//   },
+// });
 
 module.exports = conn;
